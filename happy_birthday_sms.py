da264fb2e2098bc11c291c9e946e4402405cea68
daydone <44883786+daydone@users.noreply.github.com>
2023-03-30 13:15:40 -0700

    Update happy_birthday_sms.py

--- happy_birthday_sms.py
+++ happy_birthday_sms.py
@@ -7,5 +7,51 @@
 
 #if BirthDate=CurrentDate
 #sms.PhoneNumber ("Happy Birthday "ContactsName"! I hope you have a wonderful day"")
-hello_world = "Hello World!"
-print hello_world 
+
+import os
+import sys
+import json
+import pyicloud
+from icalendar import Calendar
+from datetime import datetime
+from twilio.rest import Client
+
+# Read configuration from file
+with open('/path/to/your/config.json', 'r') as f:
+    config = json.load(f)
+
+# Twilio credentials
+account_sid = config['twilio']['account_sid']
+auth_token = config['twilio']['auth_token']
+twilio_number = config['twilio']['twilio_number']
+my_number = config['twilio']['my_number']
+
+# iCloud credentials
+username = config['icloud']['username']
+password = config['icloud']['password']
+
+# Function to send SMS alerts
+def send_sms(number, message):
+    client = Client(account_sid, auth_token)
+    message = client.messages.create(
+        to=number,
+        from_=twilio_number,
+        body=message)
+    print(f"Message sent to {number} with ID: {message.sid}")
+
+# Function to check birthdays
+def check_birthdays():
+    api = pyicloud.PyiCloudService(username, password)
+    cal = api.calendar.events()
+    today = datetime.today().strftime('%m%d')
+    for event in cal:
+        summary = event.get('summary')
+        start = event.get('startDate').strftime('%m%d')
+        if start == today and 'Birthday' in summary:
+            name = summary.split('\'s')[0].replace('Birthday ', '')
+            phone = event.get('phone', '')
+            send_sms(my_number, f"Happy Birthday, {name}!")
+            
+# Main function
+if __name__ == '__main__':
+    check_birthdays()
