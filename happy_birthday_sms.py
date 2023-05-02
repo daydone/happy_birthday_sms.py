c220256e551fcea02a4bb565f5ac71dce1c6d7e3
daydone <44883786+daydone@users.noreply.github.com>
2023-05-02 11:27:44 -0700

    Update happy_birthday_sms.py

--- happy_birthday_sms.py
+++ happy_birthday_sms.py
@@ -28,14 +28,16 @@ def send_sms(number, message):
         from_=twilio_number,
         body=message)
     print(f"Message sent to {number} with ID: {message.sid}")
-
-# Function to check birthdays
+    
+# Checks for any events in the iCloud calendar with a "Birthday" keyword in the summary that match today's date,
+# and sends a customizable SMS message to the user's specified contacts.
 def check_birthdays():
     api = pyicloud.PyiCloudService(username, password)
     cal = api.calendar.events()
     today = datetime.today().strftime('%m%d')
     include_list = config['contacts'].get('include', [])
     exclude_list = config['contacts'].get('exclude', [])
+    message_template = config['contacts'].get('message_template', 'Happy Birthday {name}!')
     for event in cal:
         summary = event.get('summary')
         start = event.get('startDate').strftime('%m%d')
@@ -43,8 +45,9 @@ def check_birthdays():
             name = summary.split('\'s')[0].replace('Birthday ', '')
             phone = event.get('phone', '')
             if name in include_list or (not exclude_list) or (name not in exclude_list):
-                send_sms(my_number, f"Happy Birthday, {name}!")
-            
+                message = message_template.format(name=name)
+                send_sms(my_number, message)
+
 # Main function
 if __name__ == '__main__':
     check_birthdays()
